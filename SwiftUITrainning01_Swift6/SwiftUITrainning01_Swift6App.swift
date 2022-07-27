a6b8f1fdce604494fbc831e70fc7fc35cf04db11
Satoshi Nishizuka <satoshi@SatoshinoMacBook-Pro.local>
2022-07-27 11:03:55 +0900

    SwiftUI練習用

---
+++ SwiftUITrainning01_Swift6/SwiftUITrainning01_Swift6App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrainning01_Swift6App.swift
+//  SwiftUITrainning01_Swift6
+//
+//  Created by Satoshi Nishizuka on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrainning01_Swift6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
